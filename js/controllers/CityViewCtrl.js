438d224e59a687ba7b663007552587f90b6c11a6
Toxcik <toxcik3110@gmail.com>
2016-07-07 23:41:27 +0300

    finished city schema, lasts example

--- js/controllers/CityViewCtrl.js
+++ js/controllers/CityViewCtrl.js
@@ -2,11 +2,34 @@ app.controller('CityViewCtrl', ['$rootScope', '$scope', '$state', '$timeout', fu
     // document.activeElement = document.getElementById('eventHandler');
     //document.getElementById('eventHandler').focus();
     $scope.armyCastle = [];
+    $scope.firstPos = 0;
     $scope.armyHero = [];
+    $scope.stockArmy = [];
+    $scope.cities = [];
     for(let i = 0; i < 8; i++) {
         $scope.armyCastle.push({id:i});
         $scope.armyHero.push({id:i});
+        $scope.stockArmy.push({id:i});
+        $scope.cities.push({id:i,color:"rgb(0,0," + i*25 +")"});
     }
     console.log(12);
 
-}]);
+    $scope.cityDown = function() {
+        if($scope.firstPos<$scope.cities.length-3)
+        $scope.firstPos++;
+    }
+
+    $scope.cityUp = function() {
+        if($scope.firstPos > 0) {
+            $scope.firstPos--;
+        }
+    }
+
+}])
+
+app.filter('onlyThreeCities', function() {
+    return function(input, a) {
+        let s = input.map((x) => x);
+        return s.splice(a,3);
+    }
+});
